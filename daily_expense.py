1df1fc8a4ef51dad0cdc49e6f5e3f681ff6a7de3
Siddharth Soni <siddharthsoni0998202@gmail.com>
2025-12-29 22:29:54 +0530

    Calculate Total and Average Expense

--- daily_expense.py
+++ daily_expense.py
@@ -4,3 +4,20 @@ expense = [120, 250, 80, 400, 150]
 
 print("Daily Expence: ")
 print(expense)
+
+# Calculate Total and Average Expense
+
+total_expense = 0
+
+for amount in expense:
+    total_expense = total_expense + amount
+
+days = len(expense)
+
+average_expense = total_expense / days 
+
+print("\nExpense Summary: ")
+print("Total Expense: ", total_expense)
+print("Number Of Days: ", days)
+print("Average Daily Expense: ", average_expense)
+
