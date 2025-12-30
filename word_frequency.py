636d7412fcc91f449fe5059d0e88ccbb6bb9c73a
Siddharth Soni <siddharthsoni0998202@gmail.com>
2025-12-30 13:41:17 +0530

    Split text itno words

--- word_frequency.py
+++ word_frequency.py
@@ -1,4 +1,13 @@
+#Text data for word frequency analysis
+
 text = "python is easy and python is powerful"
 
 print("Original Text")
 print(text)
+
+#Split Text into Words
+
+words = text.split()
+
+print("\nWord List: ")
+print(words)
