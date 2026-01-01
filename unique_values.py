a40bb7f90d0e8feb9f47a35ebfb5782135f1a84e
Siddharth Soni <siddharthsoni0998202@gmail.com>
2026-01-01 20:09:12 +0530

    Identify Unique Values

--- unique_values.py
+++ unique_values.py
@@ -13,8 +13,19 @@ print(unique_values)
 #Count Total and Unique values 
 
 total_count = len(values)
-unique_values = len(unique_values)
+unique_values_count = len(unique_values)
 
 print("\nCounts:")
 print("Total Count:", total_count)
 print("Total Unique Count:", unique_values)
+
+#Find Duplicate Values
+
+duplicate = []
+
+for value in unique_values:
+    if values.count(value) > 1:
+        duplicate.append(value)
+
+print("\nDuplicate Values")
+print(duplicate)
