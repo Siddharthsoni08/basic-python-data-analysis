5b7a677888116383fa0e7be74e3835534128c7f8
Siddharth Soni <siddharthsoni0998202@gmail.com>
2026-01-01 21:46:29 +0530

    Add Quantity Support to shopping cart

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,11 +1,11 @@
 #Shoping Cart System
 
 cart = [
-    {"name": "Apply", "price": 50},
-    {"name": "Banana", "price": 20},
-    {"name": "Orange", "price": 70},
-    {"name": "milk", "price": 20},
-    {"name": "pen", "price": 10}
+    {"name": "Apply", "price": 50, "quantity": 2},
+    {"name": "Banana", "price": 20, "quantity": 3},
+    {"name": "Orange", "price": 70, "quantity": 5},
+    {"name": "milk", "price": 20, "quantity": 7},
+    {"name": "pen", "price": 10, "quantity": 1}
 ]
 
 print("Shopping Cart Items:")
@@ -17,7 +17,16 @@ for item in cart:
 total_price = 0
 
 for item in cart:
-    total_price = total_price + item["price"]
+    total_item = item["price"] * item["quantity"]
+    total_price = total_price + total_item
+
+print("\nShopping Cart Item:")
+for item in cart:
+    print(
+        item["name"],
+        "- Price:", item["price"],
+        "| Quantity:", item["quantity"]
+    )
 
 print("\nTotal Cart Price:")
 print("₹", total_price)
