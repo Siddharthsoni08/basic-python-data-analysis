364563d2f02679296089567feb2233dc8e298860
Siddharth Soni <siddharthsoni0998202@gmail.com>
2026-01-02 21:58:15 +0530

    Add student attendance data

---
+++ attendance_system.py
@@ -0,0 +1,14 @@
+# Student Attendance System - Stage 1
+
+attendance = {
+    "Amit": "Present",
+    "Neha": "Absent",
+    "Rohit": "Present",
+    "Pooja": "Present",
+    "Rahul": "Absent"
+}
+
+print("Student Attendance:")
+for name, status in attendance.items():
+    print(name, "-", status)
+
