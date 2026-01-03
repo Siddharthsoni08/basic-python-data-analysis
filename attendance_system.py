22a479e71e7ddee459ea79201f655ab822f18181
Siddharth Soni <siddharthsoni0998202@gmail.com>
2026-01-03 10:44:10 +0530

    Count present and absent students

--- attendance_system.py
+++ attendance_system.py
@@ -12,3 +12,24 @@ print("Student Attendance:")
 for name, status in attendance.items():
     print(name, "-", status)
 
+
+
+# Count present and absent students
+
+present_count = 0
+absent_count = 0
+
+for status in attendance.values():
+    if status == "Present":
+        present_count += 1
+    else:
+        absent_count += 1
+
+total_students = len(attendance)
+
+print("\nAttendance Summary:")
+print("Total Students:", total_students)
+print("Present:", present_count)
+print("Absent:", absent_count)
+
+
